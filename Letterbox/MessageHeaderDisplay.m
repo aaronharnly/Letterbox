21f6946b10746acda0c736443b11511f6f92978e
aaronharnly <aaronharnly>
2006-06-08 04:13:23 +0000

    [internal] Added headers for several controllers and views from Mail.app. We're going to use these headers to access the scroll view in the message pane, in order to fix the bug wherein the scroll view is shifted down a few pixels.

---
+++ Letterbox/MessageHeaderDisplay.m
@@ -0,0 +1,14 @@
+//
+//  MessageHeaderDisplay.m
+//  Letterbox
+//
+//  Created by Aaron on 6/7/06.
+//  Copyright 2006 Telefirma, Inc.. All rights reserved.
+//
+
+#import "MessageHeaderDisplay.h"
+
+
+@implementation MessageHeaderDisplay
+
+@end
